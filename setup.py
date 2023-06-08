699c88ea244ebbcf1301ecb7b24277ad996dc135
FUNCER <118087385+Sudox00@users.noreply.github.com>
2023-06-08 20:01:53 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='user_avito',
+      version='0.1',
+      author='soketpy',
+      author_email='t.me/soketpy',
+      description='temp',
+      keywords = ["user_avito"],
+      install_requires = ["setuptools","requests", "bs4"],   
+      packages=find_packages()
+)
